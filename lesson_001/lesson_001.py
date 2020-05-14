850f11d3ac7eca2872a54986f48bddd1a72d590d
nestor <nestorcolt@hotmail.com>
2020-05-14 19:26:11 +0200

    end of class 1

--- lesson_001/lesson_001.py
+++ lesson_001/lesson_001.py
@@ -2,6 +2,8 @@
 
 Resources:
     https://docs.python.org/3/library/functions.html  (Python built-in functions)
+    https://www.programiz.com/python-programming/operators
+    https://www.python.org/dev/peps/pep-0008/ (PEP8 PYTHON SYNTAX STYLE GUIDE)
 
 """
 
@@ -13,29 +15,29 @@ x = 5
 y = 2
 
 # Result
-print(x + y)  # Should be 12
+# print(x + y)  # Should be 12
 
 # More than numbers:
 some_string = "Hey there!"
 other_string = 'Hey there!'
 
-print(some_string)
-print(other_string)
+# print(some_string)
+# print(other_string)
 
 # Booleans
 bool_one = True
 bool_two = False
 
-print(bool_one)
+# print(bool_one)
 
 # None
 some_var = None
-print("First Definition of some_var: ", some_var)
+# print("First Definition of some_var: ", some_var)
 
 ##############################################################################################
 # Redefining a variable
 some_var = 14
-print("Second Definition of some_var: ", some_var)
+# print("Second Definition of some_var: ", some_var)
 
 ##############################################################################################
 # Comments
@@ -50,12 +52,42 @@ in multiple lines or block of lines.
 
 """
 
-print(""" print this to the console """)
+# print(""" print this to the console """)
 
 ##############################################################################################
 # User input
-user_name = input("Please enter your name: ")
-greeting = "Hello " + user_name + "!"  # concatenation
-print(greeting)
+# user_name = input("Please enter your name: ")
+# greeting = "Hello " + user_name + "!"  # concatenation
+# print(greeting)
+
+##############################################################################################
+
+# conditionals
+
+mood = "happy"
+
+if mood == "happy":
+    # print("It is great to see you happy!")
+    pass
+
+else:
+    # print("Cheer up, mate!")
+    pass
+
+# elif
+
+mood = "happy"
+
+if mood == "happy":
+    print("It is great to see you happy!")
+
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+
+elif mood == "sad":
+    print("please don't be sad.")
+
+else:
+    print("Cheer up, mate!")
 
 ##############################################################################################
